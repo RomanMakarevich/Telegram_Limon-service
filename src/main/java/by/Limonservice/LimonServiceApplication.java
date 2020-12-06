297e31e5784442313975ec0c085f5e6212881e6d
mak <romanmak955@gmail.com>
2020-12-06 15:44:28 +0300

    Spring initializer

---
+++ src/main/java/by/Limonservice/LimonServiceApplication.java
@@ -0,0 +1,13 @@
+package by.Limonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/Limonservice/LimonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.Limonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
